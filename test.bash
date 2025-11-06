651306a0a91ac7a67dfed61b8ec3ccb3a0d34503
Issei Hanashi <ihanashi@icloud.com>
2025-11-06 12:28:30 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,13 @@
 #!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Issei Hanashi
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+       echo ${1}行目が違うよ
+       res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus_stdin)
 [ "${out}" = 15 ] || ng "$LINENO"
